eee2381e1c5a62e0e5028339142ab36ebf2efb25
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-16 16:57:11 -0400

    making classes

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+class Trainer {
+  constructor(name) {
+    this.name = name
+    this.party = []
+  }
+
+}
+
+
+
+
+class Pkmn {
+  constructor(pkdex, hp, name, atk, def, sp_atk, sp_def, spd) {
+    this.pkdex = pkdex
+    this.hp = hp
+    this.name = name
+    this.atk = atk
+    this.def = def
+    this.sp_atk = sp_atk
+    this.sp_def = sp_def
+    this.spd = spd
+  }
+}
+
+var Mimikyu = new Pkmn()
