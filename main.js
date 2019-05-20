f842084f1a5e2f0914c1e6229b8b90afcaa6fb6a
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-20 17:01:37 -0400

    AJAX Stuff and things

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-
-
+/*
+pkdex = data.id
+name = data.species.name
+spd = data.stats[0].base_stat
+sp_def = data.stats[1].base_stat
+sp_atk = data.stats[2].base_stat
+def = data.stats[3].base_stat
+atk = data.stats[4].base_stat
+hp = data.stats[5].base_stat
+*/
 
 class Trainer {
-  constructor(name) {
+  constructor(name, party) {
     this.name = name
-    this.party = []
+    this.party = party
   }
 
 }
-
-
-
+const jason = new Trainer('Jason', ['778','306','385'])
 
 class Pkmn {
-  constructor(pkdex, hp, name, atk, def, sp_atk, sp_def, spd) {
+  constructor(pkdex, name, hp, atk, def, sp_atk, sp_def, spd) {
     this.pkdex = pkdex
-    this.hp = hp
     this.name = name
+    this.hp = hp
     this.atk = atk
     this.def = def
     this.sp_atk = sp_atk
@@ -24,13 +30,18 @@ class Pkmn {
     this.spd = spd
   }
 }
-/*
-FILENAME.stats[0].base_stat = spd
-FILENAME.stats[1].base_stat = spd
-FILENAME.stats[2].base_stat = spd
-FILENAME.stats[3].base_stat = spd
-FILENAME.stats[4].base_stat = spd
-FILENAME.stats[5].base_stat = spd
-FILENAME.stats[6].base_stat = spd
-*/
-var Mimikyu = new Pkmn()
+
+function loadPkmn(pknum) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText)
+      console.log(typeof data)
+      pokemon = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat)
+    }
+  }
+  xhttp.open('GET', `https://pokeapi.co/api/v2/pokemon/${pknum}`, true)
+  xhttp.send()
+}
+
+loadPkmn(778)
