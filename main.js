87290f6bc33f8c1e15b2836c01a46dda95797bcf
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-22 16:30:59 -0400

    getting the images on screen and re-css'ing the F**k outta the bars of the left

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ class Trainer {
     this.name = name
     this.party = party
   }
+  all() {
+    console.log(this.party)
+  }
 }
 const jason = new Trainer('Jason', [778,306,385])
 console.log(jason)
@@ -38,13 +41,15 @@ function loadPkmn(pknum) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       data = JSON.parse(this.responseText)
-      console.log(typeof data)
-      if (data.types.length = 1) {
-        pknum = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat, [data.types[0].type["name"]], data.sprites.front_default)
+      // console.log(typeof data) JUST TO TEST THAT THE DATA IS NOW AN OBJECT
+      if (data.types.length = 2) {
+        pokemon = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat, [data.types[0].type["name"], data.types[1].type["name"]], `https://img.pokemondb.net/sprites/sun-moon/icon/${data.species.name}.png`)
         jason.party.push(pknum)
       } else {
-        pknum = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat, [data.types[0].type["name"], data.types[1].type["name"]], data.sprites.front_default)
+        pokemon = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat, [data.types[0].type["name"]], `https://img.pokemondb.net/sprites/sun-moon/icon/${data.species.name}.png`)
+        jason.party.push(pknum)
       }
+      console.log(pokemon)
     }
   }
   xhttp.open('GET', `https://pokeapi.co/api/v2/pokemon/${pknum}`, true)
@@ -60,7 +65,6 @@ for (team in jason.party) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       data = JSON.parse(this.responseText)
-      console.log(typeof data)
       pokemon = new Pkmn(data.id, data.species.name, data.stats[5].base_stat, data.stats[4].base_stat, data.stats[3].base_stat, data.stats[2].base_stat, data.stats[1].base_stat, data.stats[0].base_stat, [data.types[0].type["name"], data.types[1].type["name"]], `https://img.pokemondb.net/sprites/sun-moon/icon/${data.species.name}.png`)
       jason.party.push(pokemon)
       jason.party.shift()
@@ -69,7 +73,7 @@ for (team in jason.party) {
   }
   xhttp.open('GET', `https://pokeapi.co/api/v2/pokemon/${jason.party[count]}`, true)
   xhttp.send()
-  console.log(count)
+  // console.log(count) JUST TO CHECK THAT THE COUNT WORKS
   count++
 }
 }
@@ -77,6 +81,9 @@ for (team in jason.party) {
 jasonPkmn()
 jasonPkmn()
 jasonPkmn()
+// 
+// let pokemonImgLoad = document.createElement('img')
+// pokemonImgLoad.setAttribute('src', `${jason.party[count].sprite}`)
 
 partyPkmn = document.getElementsByClassName('pSlider')
-partyPkmn[0].setAttribute
+partyPkmn[0].classList.add('loaded')
